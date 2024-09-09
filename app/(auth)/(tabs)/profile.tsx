363c008769af73bf201b531e2c986f0fa65e92de
Nahuel Hernández <jnahuelhernandez@gmail.com>
2024-09-09 14:21:16 +0100

    feat: check brigade component

--- app/(auth)/(tabs)/profile.tsx
+++ app/(auth)/(tabs)/profile.tsx
@@ -9,6 +9,7 @@ import useAxios from "axios-hooks";
 import { SimpleChip, ProgressBar } from "@/components/themed";
 import { deserialize, ExistingDocumentObject } from "jsonapi-fractal";
 import { BaseObject, ErrorResponse, Team, TEAM_LEADER_ROLE } from "@/schema";
+import { CheckTeam } from "@/components/segments/CheckTeam";
 
 // example JSON Data
 const visitsData = {
@@ -55,88 +56,92 @@ export default function TabTwoScreen() {
 
   return (
     <SafeAreaView>
-      <View className="flex flex-1">
-        <ScrollView className="py-5 px-5">
-          <View className="p-4 mb-4 border border-gray-200 rounded-lg">
-            <Text className="text-md text-gray-600 mb-2">
-              Número de visitas
-            </Text>
-            <View className="flex-row items-center justify-between">
-              <Text className="text-3xl font-semibold">
-                {visitsData.visits}
+      <CheckTeam view="profile">
+        <View className="flex flex-1">
+          <ScrollView className="py-5 px-5">
+            <View className="p-4 mb-4 border border-gray-200 rounded-lg">
+              <Text className="text-md text-gray-600 mb-2">
+                Número de visitas
               </Text>
-              <SimpleChip
-                ionIcon="arrow-up"
-                label={`${visitsData.weeklyChange} esta semana`}
-              />
-            </View>
-          </View>
-
-          <View className="p-4 mb-4 border border-gray-200 rounded-lg">
-            <Text className="text-md text-gray-600 mb-2">Número de sitios</Text>
-            <View className="flex-row items-center justify-between">
-              <Text className="text-3xl font-semibold">
-                {sitesData.totalSites}
-              </Text>
-
-              <SimpleChip
-                ionIcon="arrow-up"
-                label={`${sitesData.weeklyChange} esta semana`}
-              />
-            </View>
-            <View className="flex flex-col mt-6">
-              <ProgressBar
-                label="Sitios Verdes"
-                progress={sitesData.greenSites}
-                color="primary"
-              />
-              <ProgressBar
-                label="Sitios Amarillos"
-                progress={sitesData.yellowSites}
-                color="yellow"
-              />
-              <ProgressBar
-                label="Sitios Rojos"
-                progress={sitesData.redSites}
-                color="red-500"
-              />
-            </View>
-          </View>
-
-          <View className="rounded-lg border border-gray-200 mb-8">
-            <View className="bg-gray-100 border-b border-gray-200 rounded-t-lg px-4 py-4">
-              <Text className="text-gray-600 font-medium">Participantes</Text>
-            </View>
-
-            {team?.members?.map((member, index) => (
-              <View
-                key={member.id}
-                className={`flex-row items-center p-4 border-gray-200 ${index === team?.members?.length - 1 ? "border-b-0 rounded-b-xl" : "border-b"}`}
-              >
-                <View className="flex items-center justify-center w-10 h-10 rounded-full bg-green-100 mr-3">
-                  <Text className="font-bold text-sm text-green-700">
-                    {getInitials(member.fullName)}
-                  </Text>
-                </View>
-                <View className="flex-1">
-                  <Text>{member.fullName}</Text>
-                </View>
-                {member.rol === TEAM_LEADER_ROLE && (
-                  <SimpleChip
-                    padding="small"
-                    textColor="blue-500"
-                    border="1"
-                    borderColor="blue-400"
-                    backgroundColor="blue-100"
-                    label={`${"Facilitador"}`}
-                  />
-                )}
+              <View className="flex-row items-center justify-between">
+                <Text className="text-3xl font-semibold">
+                  {visitsData.visits}
+                </Text>
+                <SimpleChip
+                  ionIcon="arrow-up"
+                  label={`${visitsData.weeklyChange} esta semana`}
+                />
               </View>
-            ))}
-          </View>
-          <View className="h-6"></View>
-        </ScrollView>
-      </View>
+            </View>
+
+            <View className="p-4 mb-4 border border-gray-200 rounded-lg">
+              <Text className="text-md text-gray-600 mb-2">
+                Número de sitios
+              </Text>
+              <View className="flex-row items-center justify-between">
+                <Text className="text-3xl font-semibold">
+                  {sitesData.totalSites}
+                </Text>
+
+                <SimpleChip
+                  ionIcon="arrow-up"
+                  label={`${sitesData.weeklyChange} esta semana`}
+                />
+              </View>
+              <View className="flex flex-col mt-6">
+                <ProgressBar
+                  label="Sitios Verdes"
+                  progress={sitesData.greenSites}
+                  color="primary"
+                />
+                <ProgressBar
+                  label="Sitios Amarillos"
+                  progress={sitesData.yellowSites}
+                  color="yellow"
+                />
+                <ProgressBar
+                  label="Sitios Rojos"
+                  progress={sitesData.redSites}
+                  color="red-500"
+                />
+              </View>
+            </View>
+
+            <View className="rounded-lg border border-gray-200 mb-8">
+              <View className="bg-gray-100 border-b border-gray-200 rounded-t-lg px-4 py-4">
+                <Text className="text-gray-600 font-medium">Participantes</Text>
+              </View>
+
+              {team?.members?.map((member, index) => (
+                <View
+                  key={member.id}
+                  className={`flex-row items-center p-4 border-gray-200 ${index === team?.members?.length - 1 ? "border-b-0 rounded-b-xl" : "border-b"}`}
+                >
+                  <View className="flex items-center justify-center w-10 h-10 rounded-full bg-green-100 mr-3">
+                    <Text className="font-bold text-sm text-green-700">
+                      {getInitials(member.fullName)}
+                    </Text>
+                  </View>
+                  <View className="flex-1">
+                    <Text>{member.fullName}</Text>
+                  </View>
+                  {member.rol === TEAM_LEADER_ROLE && (
+                    <SimpleChip
+                      padding="small"
+                      textColor="blue-500"
+                      border="1"
+                      borderColor="blue-400"
+                      backgroundColor="blue-100"
+                      label={`${"Facilitador"}`}
+                    />
+                  )}
+                </View>
+              ))}
+            </View>
+            <View className="h-6"></View>
+          </ScrollView>
+        </View>
+      </CheckTeam>
     </SafeAreaView>
   );
 }
--- app/(auth)/(tabs)/visits.tsx
+++ app/(auth)/(tabs)/visits.tsx
@@ -1,30 +1,35 @@
 import { StyleSheet } from "react-native";
 
-import { Text, View } from "@/components/themed";
+import { Text, View, SafeAreaView } from "@/components/themed";
 import Button from "@/components/themed/Button";
 import { useTranslation } from "react-i18next";
 
 import { useRouter } from "expo-router";
+import { CheckTeam } from "@/components/segments/CheckTeam";
 
 export default function TabTwoScreen() {
   const { t } = useTranslation();
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{t("tabs.visit")}</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <Button
-        primary
-        className="w-1/2 mt-6"
-        title={t("visit.start")}
-        onPress={() => router.push("select-house")}
-      />
-    </View>
+    <SafeAreaView>
+      <CheckTeam view="visit">
+        <View style={styles.container}>
+          <Text style={styles.title}>{t("tabs.visit")}</Text>
+          <View
+            style={styles.separator}
+            lightColor="#eee"
+            darkColor="rgba(255,255,255,0.1)"
+          />
+          <Button
+            primary
+            className="w-1/2 mt-6"
+            title={t("visit.start")}
+            onPress={() => router.push("select-house")}
+          />
+        </View>
+      </CheckTeam>
+    </SafeAreaView>
   );
 }
 
