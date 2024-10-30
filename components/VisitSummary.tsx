8c443a0a6dff77ef744e2ea76f44be45076bc9fe
zant <gonzalochristobal@gmail.com>
2024-10-30 21:48:13 +0100

    feat: add i18n

--- components/VisitSummary.tsx
+++ components/VisitSummary.tsx
@@ -1,32 +1,69 @@
 import HouseWarning from "@/assets/images/icons/house-warning.svg";
+import Ring from "@/assets/images/icons/ring.svg";
+import Stoplight from "@/assets/images/icons/stoplight.svg";
+import { Text, View } from "@/components/themed";
+import { createElement } from "react";
+import { useTranslation } from "react-i18next";
 import { StyleSheet } from "react-native";
 import CircularProgress from "react-native-circular-progress-indicator";
-import { Text, View } from "@/components/themed";
 
-const styles = StyleSheet.create({ circle: { backgroundColor: "#FC0606" } });
+const StatusAssets = {
+  red: {
+    color: "#FC0606",
+    image: HouseWarning,
+  },
+  green: {
+    color: "#00A300",
+    image: Stoplight,
+  },
+  yellow: {
+    color: "#FCC914",
+    image: Ring,
+  },
+};
 
 export interface VisitSummaryProps {
   date: string;
+  color: keyof typeof StatusAssets;
   sector?: string;
   house?: string;
 }
 
-const VisitSummary = ({ date, sector, house }: VisitSummaryProps) => {
+const IconStatus = ({ color }: { color: keyof typeof StatusAssets }) => {
+  const styles = StyleSheet.create({
+    circle: { backgroundColor: StatusAssets[color].color },
+  });
+
+  return (
+    <View
+      className="rounded-full w-20 h-20 flex items-center justify-center"
+      style={[styles.circle]}
+    >
+      {createElement(StatusAssets[color].image)}
+    </View>
+  );
+};
+
+const VisitSummary = ({ date, sector, house, color }: VisitSummaryProps) => {
+  const { t } = useTranslation();
+
   return (
     <View className="flex px-2">
       <View className="border-b border-neutral-200 flex flex-row justify-center justify-around py-6 flex flex-col mb-8">
-        <Text className="font-semibold text-xl mb-8">Estado del sitio</Text>
+        <Text className="font-semibold text-xl mb-8">
+          {t("visit.summary.siteStatus")}
+        </Text>
         <View className="flex flex-row gap-4 mb-4">
-          <View
-            className="rounded-full w-20 h-20 flex items-center justify-center"
-            style={[styles.circle]}
-          >
-            <HouseWarning />
+          <View>
+            <IconStatus color={color} />
           </View>
           <View className="justify-center">
-            <Text className="font-semibold text-xl">Sitio rojo</Text>
+            <Text className="font-semibold text-xl">
+              {t("visit.summary.site")}{" "}
+              {t(`visit.summary.statusColor.${color}`)}
+            </Text>
             <Text className="font-normal text-base opacity-80 text">
-              Estado del sitio al día de hoy
+              {t("visit.summary.description")}
             </Text>
           </View>
         </View>
@@ -34,12 +71,12 @@ const VisitSummary = ({ date, sector, house }: VisitSummaryProps) => {
 
       <View className="mb-4">
         <Text className="font-semibold text-xl">
-          Estado de los contenedores
+          {t("visit.summary.containerStatus")}
         </Text>
         <View className="flex align-center flex-row py-6 justify-between border-b border-neutral-200 mb-6">
           <View className="flex flex-1 items-center justify-center ">
             <CircularProgress
-              value={4}
+              value={1}
               maxValue={4}
               radius={35}
               duration={0}
@@ -48,12 +85,12 @@ const VisitSummary = ({ date, sector, house }: VisitSummaryProps) => {
               inActiveStrokeWidth={8}
             />
             <Text className="mt-2 text-center" type="small">
-              Verdes
+              {t("visit.summary.statusColor.green")}
             </Text>
           </View>
           <View className="flex flex-1 items-center justify-center">
             <CircularProgress
-              value={4}
+              value={0}
               maxValue={4}
               radius={35}
               duration={0}
@@ -62,13 +99,13 @@ const VisitSummary = ({ date, sector, house }: VisitSummaryProps) => {
               inActiveStrokeWidth={8}
             />
             <Text className="mt-2 text-center" type="small">
-              Amarillos
+              {t("visit.summary.statusColor.yellow")}
             </Text>
           </View>
           <View className="flex flex-1 items-center justify-center">
             <CircularProgress
               duration={0}
-              value={1}
+              value={0}
               maxValue={5}
               radius={35}
               activeStrokeWidth={8}
@@ -76,7 +113,7 @@ const VisitSummary = ({ date, sector, house }: VisitSummaryProps) => {
               activeStrokeColor="#FC0606"
             />
             <Text className="mt-2 text-center" type="small">
-              Rojos
+              {t("visit.summary.statusColor.red")}
             </Text>
           </View>
         </View>
