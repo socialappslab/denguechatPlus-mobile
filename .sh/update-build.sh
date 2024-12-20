b174fde34f9e139503f1ef4949ad32732203d915
zant <gonzalochristobal@gmail.com>
2024-12-20 17:08:02 +0100

    feat: update env

--- .sh/update-build.sh
+++ .sh/update-build.sh
@@ -1,5 +1,8 @@
 # env
 rm .env.production && mv .env.development .env
+echo $EXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN
+echo \n >> .env
+echo EXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN=$EXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN >> .env
 
 yarn install
 eas build --platform android --profile development --non-interactive
