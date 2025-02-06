c746cd6428b069abcc370646942dc06573c34750
zant <gonzalochristobal@gmail.com>
2025-02-06 15:28:07 +0100

    chore: log .env

--- .sh/update-build.sh
+++ .sh/update-build.sh
@@ -2,7 +2,7 @@
 touch .env
 rm .env.production && mv .env.development .env
 
-echo .env
+cat .env
 # add rollbar
 printf "\nEXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN=$EXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN" >> .env
 
