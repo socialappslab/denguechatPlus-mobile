eb77514bc343dbab8a5562d840220171b0a09b50
zant <gonzalochristobal@gmail.com>
2025-02-06 16:09:07 +0100

    chore: rely on expo environments

--- .sh/update-build.sh
+++ .sh/update-build.sh
@@ -1,10 +1,2 @@
-# env
-touch .env
-rm .env.production && mv .env.development .env
-
-cat .env
-# add rollbar
-printf "\nEXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN=$EXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN" >> .env
-
 yarn install
 eas build --platform all --profile development --non-interactive
