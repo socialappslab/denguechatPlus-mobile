206e0ba109c26da7baf9c1985b8d19e3ad60c87e
zant <gonzalochristobal@gmail.com>
2025-02-10 19:43:10 +0100

    chore: set production pipeline

--- .sh/eas-build-publish.sh
+++ .sh/eas-build-publish.sh
@@ -6,17 +6,5 @@ elif [[ $GITHUB_REF_NAME == *"v."* ]]; then
      SUBMIT_PROFILE="internal_test"
 fi
 
-# env
-touch .env
-
-if [[ $BUILD_PROFILE == "production" ]]; then
-    mv .env.production .env
-else
-    mv .env.development .env
-fi
-
-# add rollbar
-echo "\nEXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN=$EXPO_PUBLIC_CLIENT_ITEM_ACCESS_TOKEN" >> .env
-
 yarn install
 eas build --platform android --profile $BUILD_PROFILE --non-interactive
