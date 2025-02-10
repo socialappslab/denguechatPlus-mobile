626797a61ebad0567f0ed8af7c781dbb883cf83b
zant <gonzalochristobal@gmail.com>
2025-02-10 19:48:29 +0100

    chore: bump version

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     name: IS_DEV ? "DengueChatPlus (Dev)" : "DengueChatPlus",
     slug: "dengue-chat-plus",
     scheme: "org.denguechat.plus",
-    version: "1.1.1",
+    version: "1.1.2",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
