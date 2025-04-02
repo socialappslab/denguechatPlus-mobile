2949a706b22af58c06eff4faa6b3c350d8c039fe
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2025-04-02 16:57:30 -0300

    chore: bump version to 1.3.0

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "DengueChatPlus",
     slug: "dengue-chat-plus",
     scheme: "org.denguechat.plus",
-    version: "1.2.0",
+    version: "1.3.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
