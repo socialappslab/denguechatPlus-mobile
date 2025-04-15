socialappslab/denguechatPlus-mobile

commit 7839f4cbd3ce5686bb0853607cbf59fd45a5a0be

fix: unexpected white background on selectable items

components/themed/SelectableItem.tsx
@@ -1,6 +1,3 @@
-// Copyright © 2024 650 Industries.
-// from expo-checkbox
-
 import CheckboxIcon from "@/assets/images/checkbox.svg";
 import { Text } from "./Text";
 import { View } from "./View";
@@ -16,11 +13,6 @@ import {
 import { Image } from "expo-image";
 import { SimpleChip } from "./SimpleChip";
 import { TextInput } from "./TextInput";
-
-// image fallback
-// <View className="bg-green-300 h-52 flex-grow mb-4 rounded-xl border-green-300 flex items-center justify-center">
-//   <Text className="text-center text">Imagen</Text>
-// </View>
 
 export function SelectableItem({
   color,
@@ -110,9 +102,9 @@ export function SelectableItem({
           >
             {checked && checkedView}
           </Pressable>
-          <View>
+          <View className="bg-transparent">
             {label && (
-              <Text className="text-sky-400 font-medium text-sm flex-grow pr-6">
+              <Text className="text-sky-400 font-medium text-sm flex-grow">
                 {label}
                 {required && "*"}
               </Text>

commit 6199ba3f71b3c4bee7c9898dc657560eab37c5e4

fix: visual regression introduced in 7839f4cb

components/themed/SelectableItem.tsx
@@ -104,7 +104,7 @@ export function SelectableItem({
           </Pressable>
           <View className="bg-transparent">
             {label && (
-              <Text className="text-sky-400 font-medium text-sm flex-grow">
+              <Text className="text-sky-400 font-medium text-sm flex-grow pr-6">
                 {label}
                 {required && "*"}
               </Text>