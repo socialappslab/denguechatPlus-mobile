25d4c80e25a34323fc4edf1426a2fffe0ff47e77
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2025-08-01 18:56:35 -0500

    chore: bump app version to 1.8.1

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ const config: ExpoConfig = {
   name: "DengueChatPlus",
   slug: "dengue-chat-plus",
   scheme: "org.denguechat.plus",
-  version: "1.8.0",
+  version: "1.8.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   newArchEnabled: false,
