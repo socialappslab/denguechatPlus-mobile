socialappslab/denguechatPlus-mobile

commit 1c0048396c50484530b1530f0a16570da3c200ad

refactor: migrate all things related to auth to zustand, reduce complexity
- Creates a new zustand store with a custom storage engine for handling
session data (access/refresh tokens) in a secure way.
- Migrates auth to zustand.
- Updates auth and routes setup to use new expo's route guards (sdk 53).
- Migrates a bunch of API calls to TanStack Query.
- Simplifies axios setup from 2 to 1 instances.
- Removes a bunch of no longer necessary code.

config/axios.ts
@@ -1,32 +1,17 @@
-import axios, {
-  AxiosError,
-  AxiosInstance,
-  AxiosRequestConfig,
-  HttpStatusCode,
-} from "axios";
-import createAuthRefreshInterceptor from "axios-auth-refresh";
-import { configure, makeUseAxios } from "axios-hooks";
-import * as SecureStore from "expo-secure-store";
+import ax, { AxiosError, HttpStatusCode } from "axios";
 import * as Application from "expo-application";
-import { router } from "expo-router";
 
-import {
-  ACCESS_TOKEN_LOCAL_STORAGE_KEY,
-  REFRESH_TOKEN_LOCAL_STORAGE_KEY,
-  USER_LOCAL_STORAGE_KEY,
-} from "@/constants/Keys";
-import { extractAxiosErrorData } from "@/util";
 import { Alert, Linking, Platform } from "react-native";
 import i18n from "./i18n";
+import invariant from "tiny-invariant";
+import useSessionStore from "@/hooks/useSessionStore";
 
-interface RetryConfig extends AxiosRequestConfig {
-  retry: number;
-  retryDelay: number;
-}
+invariant(
+  process.env.EXPO_PUBLIC_API_URL,
+  "Expected EXPO_PUBLIC_API_URL to be defined",
+);
 
-export const globalConfig: RetryConfig = {
-  retry: 0,
-  retryDelay: 1000,
+const axios = ax.create({
   baseURL: `${process.env.EXPO_PUBLIC_API_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
@@ -34,36 +19,16 @@ export const globalConfig: RetryConfig = {
     "X-Client-Device": "mobile",
     "X-App-Version": Application.nativeApplicationVersion,
   },
-};
+});
 
-export const authApi = axios.create(globalConfig);
-
-export async function removeUser() {
-  await SecureStore.deleteItemAsync(USER_LOCAL_STORAGE_KEY);
-  await SecureStore.deleteItemAsync(ACCESS_TOKEN_LOCAL_STORAGE_KEY);
-  await SecureStore.deleteItemAsync(REFRESH_TOKEN_LOCAL_STORAGE_KEY);
-}
-
-export async function saveAccessToken(accessToken: string) {
-  await SecureStore.setItemAsync(ACCESS_TOKEN_LOCAL_STORAGE_KEY, accessToken);
-}
-
-export const resetAuthApi = async () => {
-  if (globalConfig.headers) {
-    delete globalConfig.headers["X-Authorization"];
-  }
-  delete authApi.defaults.headers["X-Authorization"];
-  await removeUser();
-};
-
-export const setAccessTokenToHeaders = (accessToken: string | null) => {
-  authApi.defaults.headers["X-Authorization"] = `${accessToken}`;
-};
-
-export const setHeaderFromLocalStorage = async () => {
-  const token = await SecureStore.getItemAsync(ACCESS_TOKEN_LOCAL_STORAGE_KEY);
-  setAccessTokenToHeaders(token);
-};
+axios.interceptors.request.use(
+  (config) => {
+    const { session } = useSessionStore.getState();
+    if (session) config.headers["X-Authorization"] = `${session.access}`;
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 /**
  * Variable to avoid showing the alert multiple times due to multiple requests
@@ -71,160 +36,48 @@ export const setHeaderFromLocalStorage = async () => {
  * interceptor itself because it's recreated on each request.
  */
 let isAlertAlreadyOpen = false;
-function setupUpgradeRequiredInterceptor(axiosInstance: AxiosInstance) {
-  axiosInstance.interceptors.response.use(undefined, (error) => {
-    if (
-      error instanceof AxiosError &&
-      error.status === HttpStatusCode.UpgradeRequired &&
-      !isAlertAlreadyOpen
-    ) {
-      isAlertAlreadyOpen = true;
+axios.interceptors.response.use(undefined, (error) => {
+  if (
+    error instanceof AxiosError &&
+    error.status === HttpStatusCode.UpgradeRequired &&
+    !isAlertAlreadyOpen
+  ) {
+    isAlertAlreadyOpen = true;
 
-      Alert.alert(
-        i18n.t("errorCodes.mustUpdateAppTitle"),
-        i18n.t("errorCodes.mustUpdateAppDescription"),
-        [
-          {
-            onPress: async () => {
-              switch (Platform.OS) {
-                case "android":
-                  await Linking.openURL(
-                    "https://play.google.com/store/apps/details?id=org.denguechatplus",
-                  );
-                  break;
-                case "ios":
-                  await Linking.openURL(
-                    "https://apps.apple.com/us/app/denguechatplus/id6741427309",
-                  );
-                  break;
-                default:
-                  console.error("Platform not supported");
-                  break;
-              }
+    Alert.alert(
+      i18n.t("errorCodes.mustUpdateAppTitle"),
+      i18n.t("errorCodes.mustUpdateAppDescription"),
+      [
+        {
+          onPress: async () => {
+            switch (Platform.OS) {
+              case "android":
+                await Linking.openURL(
+                  "https://play.google.com/store/apps/details?id=org.denguechatplus",
+                );
+                break;
+              case "ios":
+                await Linking.openURL(
+                  "https://apps.apple.com/us/app/denguechatplus/id6741427309",
+                );
+                break;
+              default:
+                console.error("Platform not supported");
+                break;
+            }
 
-              isAlertAlreadyOpen = false;
-            },
-            text: i18n.t("update"),
+            isAlertAlreadyOpen = false;
           },
-        ],
-        { cancelable: false },
-      );
-    }
-
-    return Promise.reject(error);
-  });
-}
-
-authApi.interceptors.response.use(
-  (response) => response,
-  async (error: AxiosError) => {
-    const config = error.config as RetryConfig;
-
-    if (!config) {
-      return Promise.reject(error);
-    }
-
-    const delayRetryRequest = new Promise<void>((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, config.retryDelay || 1000);
-    });
-
-    if (!config.retry) {
-      return Promise.reject(error);
-    }
-
-    config.retry -= 1;
-    return delayRetryRequest.then(() => authApi(config));
-  },
-);
-
-export const publicApi = axios.create(globalConfig);
-
-export const useAxiosNoAuth = makeUseAxios({
-  axios: publicApi,
-});
-
-export async function getRefreshToken() {
-  return await SecureStore.getItemAsync(REFRESH_TOKEN_LOCAL_STORAGE_KEY);
-}
-
-// Function that will be called to refresh authorization
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const refreshAuthLogic = async (failedRequest: any) => {
-  const refreshToken = await getRefreshToken();
-  // console.log("refreshAuthLogic with refresh>>>>>>", refreshToken);
-  return publicApi
-    .post(
-      "/users/session/refresh_token",
-      {},
-      {
-        headers: {
-          "X-Refresh-Token": refreshToken,
+          text: i18n.t("update"),
         },
-      },
-    )
-    .then((refreshResult) => {
-      const newToken = refreshResult.data?.meta?.jwt?.res?.access;
-      console.log("refreshResult newToken", newToken);
-
-      if (!newToken) {
-        return Promise.reject();
-      }
-      // eslint-disable-next-line no-param-reassign
-      failedRequest.response.config.headers["X-Authorization"] = `${newToken}`;
-      setAccessTokenToHeaders(newToken);
-      saveAccessToken(newToken);
-      return Promise.resolve();
-    })
-    .catch((error) => {
-      console.log("error refreshAuthLogic", JSON.stringify(error));
-      router.replace("/logout");
-
-      return Promise.reject();
-    });
-};
-
-createAuthRefreshInterceptor(authApi, refreshAuthLogic, {
-  statusCodes: [401],
-  shouldRefresh: (error) => {
-    const { config } = error;
-    if (config?.url?.endsWith("refresh_token")) {
-      return false;
-    }
-    const errorData = extractAxiosErrorData(error);
-
-    console.log(
-      "shouldRefresh method url>>>>>>",
-      error.response?.status +
-        " " +
-        error.response?.config?.method +
-        " " +
-        error.response?.config?.url +
-        " " +
-        JSON.stringify(error.response?.config?.params),
+      ],
+      { cancelable: false },
     );
-    if (
-      errorData?.errors &&
-      `${errorData?.errors[0]?.error_code}` === "expired_token"
-    ) {
-      return true;
-    }
+  }
 
-    return false;
-  },
-  onRetry: (requestConfig) => {
-    console.log(
-      "onRetry url >>>>>>",
-      requestConfig.url + " " + requestConfig?.headers,
-    );
-
-    return requestConfig;
-  },
-  pauseInstanceWhileRefreshing: true,
+  return Promise.reject(error);
 });
 
-configure({ axios: authApi, cache: false });
+// TODO: Implement refresh token logic
 
-setupUpgradeRequiredInterceptor(authApi);
-setupUpgradeRequiredInterceptor(publicApi);
+export { axios };

app/(auth)/(chat)/edit-post.tsx
@@ -27,7 +27,6 @@ import {
   PostVisibility,
   Team,
 } from "@/schema";
-import { useAuth } from "@/context/AuthProvider";
 
 import {
   Text,
@@ -43,14 +42,15 @@ import { getInitialsBase } from "@/util";
 import { Button } from "@/components/themed";
 import { ClosableBottomSheet } from "@/components/themed/ClosableBottomSheet";
 import Media from "@/components/icons/Media";
-import { authApi } from "@/config/axios";
+import { axios } from "@/config/axios";
 import { Post } from "@/types";
 import DeleteSmall from "@/components/icons/DeleteSmall";
+import { useStore } from "@/hooks/useStore";
 
 export default function EditPost() {
   const { t } = useTranslation();
   const isFocused = useIsFocused();
-  const { meData } = useAuth();
+  const userProfile = useStore((state) => state.userProfile);
   const router = useRouter();
   const [selectedPhoto, setSelectedPhoto] =
     useState<ImagePicker.ImagePickerAsset>();
@@ -155,7 +155,7 @@ export default function EditPost() {
     }
 
     try {
-      const response = await authApi.put(`posts/${post?.id}`, form, {
+      const response = await axios.put(`posts/${post?.id}`, form, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -227,8 +227,8 @@ export default function EditPost() {
   };
 
   const initials = getInitialsBase(
-    String(meData?.userProfile?.firstName),
-    String(meData?.userProfile?.lastName),
+    String(userProfile?.userProfile?.firstName),
+    String(userProfile?.userProfile?.lastName),
   );
 
   return (
@@ -250,9 +250,9 @@ export default function EditPost() {
                   </Text>
                 </View>
                 <View className="flex flex-1 flex-col">
-                  <Text className="font-semibold">{`${meData?.userProfile?.firstName} ${meData?.userProfile?.lastName}`}</Text>
+                  <Text className="font-semibold">{`${userProfile?.userProfile?.firstName} ${userProfile?.userProfile?.lastName}`}</Text>
                   <Text className={`text-sm opacity-60`}>
-                    {(meData?.userProfile?.team as Team)?.sector_name}
+                    {(userProfile?.userProfile?.team as Team)?.sector_name}
                   </Text>
                 </View>
 

app/(auth)/(chat)/new-post.tsx
@@ -19,7 +19,6 @@ import { useIsFocused } from "@react-navigation/native";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 
 import { postSchema, PostInputType, PostVisibility, Team } from "@/schema";
-import { useAuth } from "@/context/AuthProvider";
 import {
   Text,
   View,
@@ -34,13 +33,14 @@ import { getInitialsBase } from "@/util";
 import { Button } from "@/components/themed";
 import { ClosableBottomSheet } from "@/components/themed/ClosableBottomSheet";
 import Media from "@/components/icons/Media";
-import { authApi } from "@/config/axios";
+import { axios } from "@/config/axios";
 import DeleteSmall from "@/components/icons/DeleteSmall";
+import { useStore } from "@/hooks/useStore";
 
 export default function NewPost() {
   const { t } = useTranslation();
   const isFocused = useIsFocused();
-  const { meData } = useAuth();
+  const userProfile = useStore((state) => state.userProfile);
   const router = useRouter();
   const [selectedPhoto, setSelectedPhoto] =
     useState<ImagePicker.ImagePickerAsset>();
@@ -109,7 +109,7 @@ export default function NewPost() {
     form.append("visibility", visibility);
 
     try {
-      const response = await authApi.post("posts", form, {
+      const response = await axios.post("posts", form, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -187,8 +187,8 @@ export default function NewPost() {
   };
 
   const initials = getInitialsBase(
-    String(meData?.userProfile?.firstName),
-    String(meData?.userProfile?.lastName),
+    String(userProfile?.userProfile?.firstName),
+    String(userProfile?.userProfile?.lastName),
   );
 
   const handleRemoveMedia = () => {
@@ -214,9 +214,9 @@ export default function NewPost() {
                   </Text>
                 </View>
                 <View className="flex flex-1 flex-col">
-                  <Text className="font-semibold">{`${meData?.userProfile?.firstName} ${meData?.userProfile?.lastName}`}</Text>
+                  <Text className="font-semibold">{`${userProfile?.userProfile?.firstName} ${userProfile?.userProfile?.lastName}`}</Text>
                   <Text className={`text-sm opacity-60`}>
-                    {(meData?.userProfile?.team as Team)?.sector_name}
+                    {(userProfile?.userProfile?.team as Team)?.sector_name}
                   </Text>
                 </View>
 

app/(auth)/(config)/change-brigade.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/themed";
 import { IUser } from "@/schema/auth";
 
-import { authApi } from "@/config/axios";
+import { axios } from "@/config/axios";
 import { BaseObject } from "@/schema";
 import { useBrigades } from "@/hooks/useBrigades";
 import { AvatarBig } from "@/components/segments/AvatarBig";
@@ -44,7 +44,7 @@ export default function ChangeBrigade() {
   const onChangeBrigade = async () => {
     setLoading(true);
     try {
-      await authApi.put(`/users/change_team`, {
+      await axios.put(`/users/change_team`, {
         teamId: selection?.newBrigade?.id,
         houseBlockId: selection?.newHouseBlock?.id,
         userId: user?.id,

app/(auth)/(config)/change-house-group.tsx
@@ -6,8 +6,7 @@ import {
   Text,
   View,
 } from "@/components/themed";
-import { authApi } from "@/config/axios";
-import { useAuth } from "@/context/AuthProvider";
+import { axios } from "@/config/axios";
 import { useRefreshOnFocus } from "@/hooks/useRefreshOnFocus";
 import { HouseBlockType, Neighborhood, Wedge } from "@/types";
 import { useMutation, useQuery } from "@tanstack/react-query";
@@ -19,6 +18,7 @@ import invariant from "tiny-invariant";
 import { useFocusEffect } from "expo-router";
 import { useNetInfo } from "@react-native-community/netinfo";
 import * as Sentry from "@sentry/react-native";
+import { useStore } from "@/hooks/useStore";
 
 function useHouseBlocksQuery(wedgeId: number | null) {
   interface HouseBlock {
@@ -57,7 +57,7 @@ function useHouseBlocksQuery(wedgeId: number | null) {
       const params = new URLSearchParams({
         "filter[wedge_id]": wedgeId!.toString(),
       });
-      return (await authApi.get(`/house_blocks?${params}`))
+      return (await axios.get(`/house_blocks?${params}`))
         .data as HouseBlockResponse;
     },
   });
@@ -66,7 +66,7 @@ function useHouseBlocksQuery(wedgeId: number | null) {
 function useChangeHouseBlockMutation() {
   return useMutation({
     mutationFn: async (houseBlockId: number) => {
-      return await authApi.put(`/users/change_house_block`, {
+      return await axios.put(`/users/change_house_block`, {
         house_block_id: houseBlockId,
       });
     },
@@ -75,12 +75,14 @@ function useChangeHouseBlockMutation() {
 
 export default function ChangeHouseBlock() {
   const { t } = useTranslation();
-  const { meData } = useAuth();
-  const router = useRouter();
   const { isInternetReachable } = useNetInfo();
 
-  // @ts-expect-error fix types
-  const wedgeId: number | null = meData?.userProfile?.team?.wedge_id ?? null;
+  const userProfile = useStore((state) => state.userProfile);
+  const router = useRouter();
+
+  const wedgeId: number | null =
+    // @ts-expect-error fix types
+    userProfile?.userProfile?.team?.wedge_id ?? null;
 
   const houseBlocks = useHouseBlocksQuery(wedgeId);
   useRefreshOnFocus(houseBlocks.refetch);
@@ -89,7 +91,7 @@ export default function ChangeHouseBlock() {
 
   const houseBlockId: string | undefined =
     // @ts-expect-error fix types
-    meData?.userProfile?.houseBlock?.id?.toString();
+    userProfile?.userProfile?.houseBlock?.id?.toString();
 
   const defaultOption = useMemo(
     () =>
@@ -170,12 +172,12 @@ export default function ChangeHouseBlock() {
             <Text>
               <Text className="font-bold">{t("config.sector")}:</Text>{" "}
               {/* @ts-expect-error fix types */}
-              {meData?.userProfile?.team?.sector_name}
+              {userProfile?.userProfile?.team?.sector_name}
             </Text>
             <Text>
               <Text className="font-bold">{t("config.wedge")}:</Text>{" "}
               {/* @ts-expect-error fix types */}
-              {meData?.userProfile?.team?.wedge_name}
+              {userProfile?.userProfile?.team?.wedge_name}
             </Text>
           </View>
 

app/(auth)/(config)/select-brigade.tsx
@@ -24,7 +24,7 @@ import {
 
 import { countSetFilters, getInitialsBase } from "@/util";
 import Colors from "@/constants/Colors";
-import { authApi } from "@/config/axios";
+import { axios } from "@/config/axios";
 
 import { useBrigades } from "@/hooks/useBrigades";
 import { Team } from "@/schema";
@@ -52,7 +52,7 @@ export default function SelectBrigade() {
   ) => {
     setError("");
     try {
-      const response = await authApi.get("teams", {
+      const response = await axios.get("teams", {
         params: {
           "page[number]": page,
           "page[size]": 15,

app/(auth)/(config)/select-user.tsx
@@ -25,7 +25,7 @@ import { IUser } from "@/schema/auth";
 
 import { countSetFilters, getInitialsBase } from "@/util";
 import Colors from "@/constants/Colors";
-import { authApi } from "@/config/axios";
+import { axios } from "@/config/axios";
 import { BaseObject } from "@/schema";
 import { useBrigades } from "@/hooks/useBrigades";
 
@@ -47,7 +47,7 @@ export default function SelectUser() {
   const fetchData = async (page: number, query: string, teamId?: number) => {
     setError("");
     try {
-      const response = await authApi.get("users", {
+      const response = await axios.get("users", {
         params: {
           "filter[status][]": "active",
           "page[number]": page,

app/(auth)/(tabs)/chat.tsx
@@ -23,14 +23,14 @@ import {
 } from "@/components/themed";
 import { Post } from "@/types";
 import Colors from "@/constants/Colors";
-import { authApi } from "@/config/axios";
+import { axios } from "@/config/axios";
 import { Team } from "@/schema";
-import { useAuth } from "@/context/AuthProvider";
 import { SimpleSelectableChip } from "@/components/themed/SelectableChip";
 import CommentsSheet from "@/components/segments/CommentsSheet";
 import { ClosableBottomSheet } from "@/components/themed/ClosableBottomSheet";
 import { ActionItem } from "@/components/themed/ActionItem";
 import { Button } from "@/components/themed";
+import { useStore } from "@/hooks/useStore";
 
 type PostState = Record<
   string,
@@ -45,7 +45,7 @@ type PostState = Record<
 
 export default function Chat() {
   const { t } = useTranslation();
-  const { meData, reFetchMe } = useAuth();
+  const userProfile = useStore((state) => state.userProfile);
 
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
@@ -75,18 +75,13 @@ export default function Chat() {
   useEffect(() => {
     if (isFocused) {
       firstLoad();
-
-      if (!meData) {
-        reFetchMe();
-      }
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isFocused, meData]);
+  }, [isFocused]);
 
   const fetchData = async (page: number, teamId?: number) => {
     setError("");
     try {
-      const response = await authApi.get("posts", {
+      const response = await axios.get("posts", {
         headers: {
           source: "mobile",
         },
@@ -174,7 +169,10 @@ export default function Chat() {
     setCurrentPage(1);
     setHasMore(true);
     setLoading(true);
-    fetchData(1, all ? undefined : (meData?.userProfile?.team as Team)?.id);
+    fetchData(
+      1,
+      all ? undefined : (userProfile?.userProfile?.team as Team)?.id,
+    );
   };
 
   const loadMoreData = () => {
@@ -185,7 +183,7 @@ export default function Chat() {
       setCurrentPage(nextPage);
       fetchData(
         nextPage,
-        all ? undefined : (meData?.userProfile?.team as Team)?.id,
+        all ? undefined : (userProfile?.userProfile?.team as Team)?.id,
       );
     }
   };
@@ -219,7 +217,10 @@ export default function Chat() {
     setDataList([]);
     setHasMore(true);
     setCurrentPage(1);
-    fetchData(1, all ? undefined : (meData?.userProfile?.team as Team)?.id);
+    fetchData(
+      1,
+      all ? undefined : (userProfile?.userProfile?.team as Team)?.id,
+    );
     setUnmountOptions(false);
   };
 
@@ -236,7 +237,7 @@ export default function Chat() {
         post.loadingLike = true;
         return newState;
       });
-      await authApi.post(`posts/${id}/like`);
+      await axios.post(`posts/${id}/like`);
       console.log("Post liked:");
       setState((prev) => {
         const newState = { ...prev };
@@ -264,7 +265,6 @@ export default function Chat() {
 
   const onPressOptions = (post: Post) => {
     setSelectedPost(post);
-    console.log("onPressOptions>>>> ", post);
     bottomSheetModalOptionsRef.current?.present();
   };
 
@@ -334,9 +334,7 @@ export default function Chat() {
   const handlePressConfirmDelete = async () => {
     try {
       setDeleteLoading(true);
-      console.log("selectedPost?.id to delete>>>>", selectedPost?.id);
-      await authApi.delete(`posts/${selectedPost?.id}`);
-      console.log("Post deleted:");
+      await axios.delete(`posts/${selectedPost?.id}`);
 
       firstLoad();
       handlePressCancel();
@@ -346,7 +344,8 @@ export default function Chat() {
         text1: t("chat.postDeleted"),
       });
     } catch (error) {
-      console.log("Error deleting post", error);
+      console.error("Error deleting post", error);
+      Sentry.captureException(error);
     } finally {
       setDeleteLoading(false);
     }
@@ -375,7 +374,7 @@ export default function Chat() {
           <SimpleSelectableChip
             label={t("chat.visibility.myBrigade")}
             checked={!all}
-            disabled={!(meData?.userProfile?.team as Team)?.id}
+            disabled={!(userProfile?.userProfile?.team as Team)?.id}
             onPressElement={handlePressMyBrigade}
           />
         </View>

commit e463144ac878dc80759ad4e545e19f24958478ef

fix: regression in 1c00483, `useAxios` stopped working

config/axios.ts
@@ -5,6 +5,7 @@ import { Alert, Linking, Platform } from "react-native";
 import i18n from "./i18n";
 import invariant from "tiny-invariant";
 import useSessionStore from "@/hooks/useSessionStore";
+import { configure } from "axios-hooks";
 
 invariant(
   process.env.EXPO_PUBLIC_API_URL,
@@ -79,5 +80,9 @@ axios.interceptors.response.use(undefined, (error) => {
 });
 
 // TODO: Implement refresh token logic
+
+// NOTE: Configure axios-hooks, remove this after migrating all requests to
+// TanStack Query
+configure({ axios, cache: false });
 
 export { axios };