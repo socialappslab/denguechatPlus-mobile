767b11ee4ec4fe5aaf4e587e4fe159760f28f4b7
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2026-04-25 15:15:58 -0300

    refactor: remove unnecessary `useCallback`s, minor changes

--- hooks/useInspectionPhotos.ts
+++ hooks/useInspectionPhotos.ts
@@ -1,4 +1,3 @@
-import { useCallback } from "react";
 import { File, Paths } from "expo-file-system";
 import { getBasenameFromFilename, getFilenameFromURI } from "@/util";
 import {
@@ -7,7 +6,7 @@ import {
   useStore,
 } from "@/hooks/useStore";
 import { VisitId } from "@/types";
-import * as Sentry from "@sentry/react-native"
+import * as Sentry from "@sentry/react-native";
 
 export function useInspectionPhotos() {
   const visitId = useStore((state) => state.visitId),
@@ -16,50 +15,42 @@ export function useInspectionPhotos() {
 
   const currentInspectionIndex = visitMetadata[visitId]?.inspectionIdx;
 
-  const attachPhotoToCurrentInspection = useCallback(
-    async (photoUri: string) => {
-      const filename = getFilenameFromURI(photoUri);
-      const source = new File(photoUri);
-      const destination = new File(Paths.document, filename);
+  async function attachPhotoToCurrentInspection(photoUri: string) {
+    const filename = getFilenameFromURI(photoUri);
+    const source = new File(photoUri);
+    const destination = new File(Paths.document, filename);
 
-      try {
-        source.copy(destination);
-      } catch (error) {
-        Sentry.captureException(error);
-        console.error(error);
-      }
+    try {
+      source.copy(destination);
+    } catch (error) {
+      Sentry.captureException(error);
+      console.error(error);
+    }
 
-      setInspectionPhoto({
-        filename,
-        uri: destination.uri,
-        visitId,
-        inspectionIdx: currentInspectionIndex,
-        referenceCode: getBasenameFromFilename(filename),
-      });
-    },
-    [currentInspectionIndex, visitId],
-  );
+    setInspectionPhoto({
+      filename,
+      uri: destination.uri,
+      visitId,
+      inspectionIdx: currentInspectionIndex,
+      referenceCode: getBasenameFromFilename(filename),
+    });
+  }
 
-  const deleteInspectionPhotosFromVisit = useCallback(
-    async (visitId: VisitId) => {
-      const photosToDelete = inspectionPhotos.filter(
-        (photo) => photo.visitId === visitId,
-      );
-      const newInspectionPhotos = inspectionPhotos.filter(
-        (photo) => photo.visitId !== visitId,
-      );
+  async function deleteInspectionPhotosFromVisit(visitId: VisitId) {
+    const photosToDelete = inspectionPhotos.filter(
+      (photo) => photo.visitId === visitId,
+    );
+    const newInspectionPhotos = inspectionPhotos.filter(
+      (photo) => photo.visitId !== visitId,
+    );
 
-      for (const photo of photosToDelete) {
-        const file = new File(photo.uri);
-
-        if (file.exists) {
-          file.delete();
-        }
-      }
-      setInspectionPhotos(newInspectionPhotos);
-    },
-    [inspectionPhotos],
-  );
+    for (const photo of photosToDelete) {
+      const file = new File(photo.uri);
+      if (!file.exists) return;
+      file.delete();
+    }
+    setInspectionPhotos(newInspectionPhotos);
+  }
 
   return {
     attachPhotoToCurrentInspection,
